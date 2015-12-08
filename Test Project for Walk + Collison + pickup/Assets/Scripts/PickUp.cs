9cd23e2d24bc98d231880f2b269a146699c9f251
tapa8728 <tapa8728@colorado.edu>
2015-12-08 01:35:47 -0700

    Test Project added

---
+++ Test Project for Walk + Collison + pickup/Assets/Scripts/PickUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision other)
+    {
+         Destroy(other.gameObject);
+        
+    }
+
+    
+}
