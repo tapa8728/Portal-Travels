775217bf97c9cf6c40bcfbe5e81f94169006023b
tapa8728 <tapa8728@colorado.edu>
2015-12-13 06:08:18 -0700

    Island Portal: Score Text to be added

--- Explore Island Portal/Assets/Scripts/PickUp.cs
+++ Explore Island Portal/Assets/Scripts/PickUp.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour
 {
+    public GUIText scoreText;
+    public int purplescore;
     void OnCollisionEnter (Collision col)
     {
         //if this works we will add more conditions on the 
         //name of the object
         if (col.gameObject.tag == "purplestar")
         {
+            purplescore = purplescore + 1;
+            UpdateScore();
             Destroy(col.gameObject);
         }
+    }
+
+    void UpdateScore()
+    {
+        //scoreText.text = "Score: " + purplescore*10;
     }
 }
