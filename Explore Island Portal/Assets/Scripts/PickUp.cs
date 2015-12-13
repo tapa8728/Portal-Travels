375ac315dc0157cddad8aaeeafd3d027e46e26b1
tapa8728 <tapa8728@colorado.edu>
2015-12-13 05:27:51 -0700

    Island Portal: Can pck up sprites

---
+++ Explore Island Portal/Assets/Scripts/PickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{
+    void OnCollisionEnter (Collision col)
+    {
+        //if this works we will add more conditions on the 
+        //name of the object
+        if (col.gameObject.tag == "purplestar")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
