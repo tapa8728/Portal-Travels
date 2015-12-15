ec4909bbd766e6d4d1de1b7a82bc06a22d0439a7
tapa8728 <tapa8728@colorado.edu>
2015-12-15 14:40:49 -0700

    Assets and Pick Up script activated

--- Island Explore Portal v2/Assets/Scripts/PickUp.cs
+++ Island Explore Portal v2/Assets/Scripts/PickUp.cs
@@ -5,6 +5,7 @@ public class PickUp : MonoBehaviour
 {
     public GUIText scoreText;
     public int purplescore;
+    public AudioClip clip;
     void OnCollisionEnter (Collision col)
     {
         //if this works we will add more conditions on the 
@@ -13,6 +14,7 @@ public class PickUp : MonoBehaviour
         {
             purplescore = purplescore + 1;
             UpdateScore();
+            AudioSource.PlayClipAtPoint(clip, transform.position);
             Destroy(col.gameObject);
         }
     }
