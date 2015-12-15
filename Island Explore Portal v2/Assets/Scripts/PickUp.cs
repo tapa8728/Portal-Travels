b05d445345c6249be4ee7dfa80c2ea069cc853c2
tapa8728 <tapa8728@colorado.edu>
2015-12-15 01:11:55 -0700

    Island Portal - Env ready

---
+++ Island Explore Portal v2/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{
+    public GUIText scoreText;
+    public int purplescore;
+    void OnCollisionEnter (Collision col)
+    {
+        //if this works we will add more conditions on the 
+        //name of the object
+        if (col.gameObject.tag == "purplestar")
+        {
+            purplescore = purplescore + 1;
+            UpdateScore();
+            Destroy(col.gameObject);
+        }
+    }
+
+    void UpdateScore()
+    {
+        //scoreText.text = "Score: " + purplescore*10;
+    }
+}
